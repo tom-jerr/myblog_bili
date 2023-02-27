6bad287db54aa5ceb4e114bfbdaaf5c7d8949252
tom-jerr <2584074296@qq.com>
2023-02-27 22:16:00 +0800

    tagcontroller修改

--- myblog_admin/src/main/java/com/uestclzy/controller/TagController.java
+++ myblog_admin/src/main/java/com/uestclzy/controller/TagController.java
@@ -19,42 +19,42 @@ public class TagController {
     @Autowired
     private TagService tagService;
 
-    @GetMapping("/list")
-    public ResponseResult<PageVo> list(Integer pageNum, Integer pageSize, TagListDto tagListDto){
-        return tagService.pageTagList(pageNum,pageSize,tagListDto);
-    }
-
-    @PostMapping
-    public ResponseResult add(@RequestBody AddTagDto tagDto){
-        Tag tag = BeanCopyUtils.copyBean(tagDto, Tag.class);
-        tagService.save(tag);
-        return ResponseResult.okResult();
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseResult delete(@PathVariable Long id){
-        tagService.removeById(id);
-        return ResponseResult.okResult();
-    }
-
-    @PutMapping
-    public ResponseResult edit(@RequestBody EditTagDto tagDto){
-        Tag tag = BeanCopyUtils.copyBean(tagDto,Tag.class);
-        tagService.updateById(tag);
-        return ResponseResult.okResult();
-    }
-
-
-    @GetMapping(value = "/{id}")
-    public ResponseResult getInfo(@PathVariable(value = "id")Long id){
-        Tag tag = tagService.getById(id);
-        return ResponseResult.okResult(tag);
-    }
-
-
-    @GetMapping("/listAllTag")
-    public ResponseResult listAllTag(){
-        List<TagVo> list = tagService.listAllTag();
-        return ResponseResult.okResult(list);
-    }
+//    @GetMapping("/list")
+//    public ResponseResult<PageVo> list(Integer pageNum, Integer pageSize, TagListDto tagListDto){
+//        return tagService.pageTagList(pageNum,pageSize,tagListDto);
+//    }
+//
+//    @PostMapping
+//    public ResponseResult add(@RequestBody AddTagDto tagDto){
+//        Tag tag = BeanCopyUtils.copyBean(tagDto, Tag.class);
+//        tagService.save(tag);
+//        return ResponseResult.okResult();
+//    }
+//
+//    @DeleteMapping("/{id}")
+//    public ResponseResult delete(@PathVariable Long id){
+//        tagService.removeById(id);
+//        return ResponseResult.okResult();
+//    }
+//
+//    @PutMapping
+//    public ResponseResult edit(@RequestBody EditTagDto tagDto){
+//        Tag tag = BeanCopyUtils.copyBean(tagDto,Tag.class);
+//        tagService.updateById(tag);
+//        return ResponseResult.okResult();
+//    }
+//
+//
+//    @GetMapping(value = "/{id}")
+//    public ResponseResult getInfo(@PathVariable(value = "id")Long id){
+//        Tag tag = tagService.getById(id);
+//        return ResponseResult.okResult(tag);
+//    }
+//
+//
+//    @GetMapping("/listAllTag")
+//    public ResponseResult listAllTag(){
+//        List<TagVo> list = tagService.listAllTag();
+//        return ResponseResult.okResult(list);
+//    }
 }
